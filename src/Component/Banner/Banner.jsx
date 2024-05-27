32fdcfa35624b7e3fc22d582769fbe3a4b6c718f
Usaid ok <usaidokdev@gmail.com>
2024-05-27 16:01:37 +0100

    Movies List add

--- src/Component/Banner/Banner.jsx
+++ src/Component/Banner/Banner.jsx
@@ -4,18 +4,21 @@ import './Banner.css'
 function Banner() {
   return (
     <>
-    <div className="banner">
-      <div className="banner-items">
-        <h2>Filmagnet</h2>
-        <h3>Unlimited <span>Entertainment,</span><br />
-        Movies, TVs shows, & More.
-        </h3>
-        <button>Play Now</button>
+      <div className="banner">
+        <div className="banner-items">
+          <h2>Filmagnet</h2>
+          <h3>Unlimited <span>Entertainment,</span><br />
+            Movies, TVs shows, & More.
+          </h3>
+          <button>Play Now</button>
+        </div>
       </div>
-    </div>
-    <div className="Movies-list">
-      
-    </div>
+      <div className="Movies-list">
+        <div className="Movies-heading">
+          <h5>ONLINE STREAMING</h5>
+          <h4>Upcoming Movies</h4>
+        </div>
+      </div>
     </>
   )
 }
