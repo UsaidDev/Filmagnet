89ed047810ee818f01f3457cf774bacc51142002
Usaid ok <usaidokdev@gmail.com>
2024-06-04 19:04:04 +0100

    Items list added

--- src/Component/DetailPage/Details.jsx
+++ src/Component/DetailPage/Details.jsx
@@ -1,21 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Details.css'
 import { useEffect } from 'react'
 import axios from '../../Constants/Axios'
-import { API_KEY } from '../../Constants/Constants'
+import { API_KEY, ImageUrl } from '../../Constants/Constants'
 function Detail(props) {
-    const ItemsId=props.Dtl_id
+
+    const [idMovie, SetidMovie] = useState([])
+    const ItemsId = props.Dtl_id
     useEffect(() => {
-        axios.get(`https://api.themoviedb.org/3/movie/${ItemsId}?api_key=${API_KEY}`).then((res)=>{
+        axios.get(`https://api.themoviedb.org/3/movie/${ItemsId}?api_key=${API_KEY}`).then((res) => {
+            SetidMovie([res.data])
             console.log(res.data)
-        }).catch((err)=>{
+        }).catch((err) => {
             console.log('Error')
         })
     }, [ItemsId])
     return (
         <div>
             <div className="Detail_Page">
-               <h2>Hello World</h2>
+                {
+                    idMovie.map((obj) => {
+                        return (<>
+                        <h2>{obj.title}</h2>
+                        <p></p>
+                        <img src={`${ImageUrl}${obj.backdrop_path}`} alt="" />
+                        </>)
+                    })
+                }
             </div>
         </div>
     )
