11cc53468d4e0bcd64dfe4ff2eeda4aafe7b8f4c
Usaid ok <usaidokdev@gmail.com>
2024-06-06 07:10:01 +0100

    Sub Movies List Completed

--- src/Component/DetailPage/Details.jsx
+++ src/Component/DetailPage/Details.jsx
@@ -1,34 +1,40 @@
-import React, { useState } from 'react'
-import './Details.css'
-import { useEffect } from 'react'
-import axios from '../../Constants/Axios'
-import { API_KEY, ImageUrl } from '../../Constants/Constants'
+import React, { useState, useEffect } from 'react';
+import './Details.css';
+import axios from '../../Constants/Axios';
+import { API_KEY, ImageUrl } from '../../Constants/Constants';
 function Detail(props) {
-
-    const [idMovie, SetidMovie] = useState([])
-    const ItemsId = props.Dtl_id
+    const [idMovie, setIdMovie] = useState([]);
+    const itemsId = props.Dtl_id;
     useEffect(() => {
-        axios.get(`https://api.themoviedb.org/3/movie/${ItemsId}?api_key=${API_KEY}`).then((res) => {
-            SetidMovie([res.data])
-            console.log(res.data)
-        }).catch((err) => {
-            console.log('Error')
-        })
-    }, [ItemsId])
+        axios.get(`https://api.themoviedb.org/3/movie/${itemsId}?api_key=${API_KEY}`)
+            .then((res) => {
+                setIdMovie([res.data]);
+                console.log(res.data);
+            })
+            .catch((err) => {
+                console.log('Error');
+            });
+    }, [itemsId]);
     return (
-        <div>
-            <div className="Detail_Page">
+        <>
                 {
-                    idMovie.map((obj) => {
-                        return (<>
-                        <h2>{obj.title}</h2>
-                        <p></p>
-                        <img src={`${ImageUrl}${obj.backdrop_path}`} alt="" />
-                        </>)
+                    idMovie.map((movie) => {
+                        return (
+                            <>
+                            <section >
+                                <img src={`${ImageUrl}${movie.backdrop_path}`} alt="" />
+                                <article id='ctn'>
+                                <h1 className='movie_name'>{movie.title}</h1>
+                                    <p className='movie_para'>{movie.overview}</p>
+                                    <h3 className='movie_release_data'>Release Date: {movie.release_date}</h3>
+                                </article>
+                                </section>
+                            </>
+                        )
                     })
                 }
-            </div>
-        </div>
-    )
+        </>
+    );
 }
+
 export default Detail;
